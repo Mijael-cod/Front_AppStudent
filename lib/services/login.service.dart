72499cc8a5426129e776d636b4b7d3e5c1b877aa
jhossepsamuel231 <jhossepsamuel23@gmail.com>
2023-11-21 00:55:02 -0500

    Update login.service.dart

--- lib/services/login.service.dart
+++ lib/services/login.service.dart
@@ -7,19 +7,23 @@ class LoginService {
     final url =
         Uri.parse('https://nestjs-pi-postgres.onrender.com/api/v1/auth/login');
         
-    final response =
-        await http.post(url, body: 
-        {'codigo': codigo, 
-        'password': password});
-
-    if (response.statusCode == 201) {
-      final data = json.decode(response.body);
-      final token = data['token'];
-      return token;
-    } else {
+      print("codigo: " + codigo + "password: " + password);
+    try {
+      final response =
+          await http.post(url, body: {'codigo': codigo, 'password': password});
+    print("codigo: " + codigo + "password: " + password);
+      if (response.statusCode == 201) {
+        final data = json.decode(response.body);
+        final token = data['token'];
+        return token;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("codigo: " + codigo + "password: " + password);
+      // Manejo de errores - puedes imprimir el error o realizar alguna otra acción
+      print('Error en la solicitud HTTP: $e');
       return null;
     }
-
   }
-
 }
