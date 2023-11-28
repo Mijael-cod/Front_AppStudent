7650fb834de25b8401ccb81d805d3fc924a6ec13
Mijael Aliaga <82177948+Mijael-cod@users.noreply.github.com>
2023-11-28 11:10:31 -0500

    icono de app

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Proyecto',
+      title: 'Help Student',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         FFLocalizationsDelegate(),
