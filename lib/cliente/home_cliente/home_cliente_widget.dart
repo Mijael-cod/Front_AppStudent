61780a2f63f5180d27d03de3888a6877d65e9858
jhossepsamuel231 <jhossepsamuel23@gmail.com>
2023-11-28 14:47:35 -0500

    k

--- lib/cliente/home_cliente/home_cliente_widget.dart
+++ lib/cliente/home_cliente/home_cliente_widget.dart
@@ -37,7 +37,7 @@ class _HomeClienteWidgetState extends State<HomeClienteWidget>
 
   String codigoperso = '';
   String _userName = ''; // Variable para almacenar el nombre de usuario
-  String _fotoPerfil = ''; // Variable para almacenar la foto
+  late Map<String, dynamic> personaDatita = {};
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -300,15 +300,16 @@ class _HomeClienteWidgetState extends State<HomeClienteWidget>
 
             if (response.statusCode == 200) {
               final personaData = json.decode(response.body);
+              personaDatita = personaData;
               final nombre =
                   personaData['nombre']; // Asegúrate de usar la clave correcta
               final apellidoPaterno = personaData['apellidoPaterno'];
               final apellidoMaterno = personaData['apellidoMaterno'];
               final foto = personaData['foto'];
+
               setState(() {
                 _userName =
                     '$nombre'; //Aca va nombre y apellido que se mostrará
-                _fotoPerfil = '$foto';
               });
             }
           }
@@ -400,8 +401,8 @@ class _HomeClienteWidgetState extends State<HomeClienteWidget>
                               ),
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(32.0),
-                                child: Image.network(
-                                  _fotoPerfil,
+                                child: Image.asset(
+                                  'assets/images/blank-profile-picture-973460_1280.png',
                                   width: 300.0,
                                   height: 200.0,
                                   fit: BoxFit.cover,
@@ -517,11 +518,19 @@ class _HomeClienteWidgetState extends State<HomeClienteWidget>
                             },
                             child: ClipRRect(
                               borderRadius: BorderRadius.circular(32.0),
-                              child: Image.asset(
-                                'assets/images/blank-profile-picture-973460_1280.png',
+                              child: Image.network(
+                                personaDatita['foto'] ?? '',
                                 width: 300.0,
                                 height: 200.0,
                                 fit: BoxFit.cover,
+                                errorBuilder: (BuildContext context,
+                                    Object exception, StackTrace? stackTrace) {
+                                  // Manejo de errores de carga de imagen
+                                  return const Placeholder(
+                                    fallbackWidth: 300.0,
+                                    fallbackHeight: 200.0,
+                                  );
+                                },
                               ),
                             ),
                           ),
